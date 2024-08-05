fcfbb808320b581742d99a38c27611a338eb63f3
unknown <dti.luis.silveira@segurossura.com.br>
2024-08-05 08:20:46 -0300

    Configuracoes iniciais

---
+++ src/main/java/com/dti/iarefinamento/IarefinamentoApplication.java
@@ -0,0 +1,13 @@
+package com.dti.iarefinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IarefinamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IarefinamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dti/iarefinamento/IarefinamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dti.iarefinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IarefinamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
